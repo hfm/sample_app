35aadc2f3149e971b7f44a12a3d899402c50e697
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-05-22 17:18:56 +0900

    Progress 3.5 Exercises No.1 "Make a Contact page for the sample app."

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,17 @@ describe "StaticPages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
