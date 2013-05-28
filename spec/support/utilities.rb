e61dca602901d7c5b663dc3949fac17be1f8cf8e
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-05-28 11:27:15 +0900

    Add spec/helpers/application_helper_spec.rb for replacing old codes in utilities.rb

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,9 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+#def full_title(page_title)
+#  base_title = "Ruby on Rails Tutorial Sample App"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end
