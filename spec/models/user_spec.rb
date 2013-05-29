ab58c3921743979e1058558af0249c2d6d111462
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-05-29 14:05:45 +0900

    Test for email "downcasing" when mixed-case-address comes.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,6 +62,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
