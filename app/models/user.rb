9ef2d8e3bce937db42a41290122f5a0e35411ffe
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-05-29 14:19:41 +0900

    Used bang method instead of usual substitution

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  has_secure_password
+  before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
