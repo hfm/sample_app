eaf2c92b6126227e5083a6576ca410c91bdaeece
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-03 17:42:19 +0900

    Used let-command to organize string of "Sign in" and "Sign out"

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,20 +4,23 @@ describe "Authentication" do
 
   subject { page }
 
+  let(:signin) { 'Sign in' }
+  let(:signout) { 'Sign out' }
+
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
+    it { should have_content(signin) }
+    it { should have_title(signin) }
   end
 
   describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
-      before { click_button "Sign in" }
+      before { click_button signin }
 
-      it { should have_title('Sign in') }
+      it { should have_title(signin) }
       it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
@@ -32,12 +35,12 @@ describe "Authentication" do
 
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_link(signout, href: signout_path) }
+      it { should_not have_link(signin, href: signin_path) }
 
       describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+        before { click_link signout }
+        it { should have_link(signin) }
       end
     end
   end
