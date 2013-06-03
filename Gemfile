188a61cee0745174e7097916af6e4d53fdc5965e
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-03 14:58:34 +0900

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ group :test do
   gem 'selenium-webdriver', '2.0'
   gem 'capybara', '2.1.0.rc1'
   gem 'factory_girl_rails', '4.2.1'
+  gem 'cucumber-rails', '1.3.0', :require => false
+  gem 'database_cleaner', '~> 1.0.0.RC1'
 end
 
 group :assets do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  #has_secure_password
   before_save { self.email.downcase! }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -10,4 +11,9 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
+
+  private
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
