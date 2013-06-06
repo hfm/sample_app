a871e8b60fe51217eea6af99fb0a0d9477302f78
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-06 14:46:44 +0900

    Successful test. $ be rspec spec/requests/user_pages_spec.rb                                                                           (exercise_9_6_1[-] @0) [~/program/rails_tutorial/sample_app] .............................
    Finished in 3.51 seconds
    29 examples, 0 failures
    
    Randomized with seed 48108

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   # Before filters
 
