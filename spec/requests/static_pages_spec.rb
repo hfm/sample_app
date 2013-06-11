267f1ff7f41fae3b10c4fa963ace6d01520637bc
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-11 19:22:50 +0900

    halfway at office

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -57,6 +57,17 @@ describe "Static pages" do
           it { should have_content(/2 microposts/) }
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
   end
 
