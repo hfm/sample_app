a6d3e4e8d790b91326f57a3501d950b8092f057d
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-12 14:34:28 +0900

    Remove duplicated test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -167,16 +167,6 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
-
-      its(:feed) { should include(newer_micropost) }
-      its(:feed) { should include(older_micropost) }
-      its(:feed) { should_not include(unfollowed_post) }
-    end
-
-    describe "status" do
-      let(:unfollowed_post) do
-        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-      end
       let(:followed_user) { FactoryGirl.create(:user) }
 
       before do
