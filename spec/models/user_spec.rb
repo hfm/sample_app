1a258f3d376ff0a617b9236a90ff393c7c9210a1
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-14 12:47:31 +0900

    Add tests for destroying relationships associated with a given user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -206,5 +206,21 @@ describe User do
       it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
     end
+
+    describe "destroyed user" do
+      it "should destroy relationships" do
+        user_id = @user.id
+        @user.destroy
+        expect(Relationship.where(follower_id:user_id ,followed_id:other_user.id)).to be_empty
+      end
+    end
+
+    describe "destroyed other_user" do
+      it "should destroy relationships" do
+        other_user_id = other_user.id
+        other_user.destroy
+        expect(Relationship.where(follower_id:@user.id ,followed_id:other_user_id)).to be_empty
+      end
+    end
   end
 end
