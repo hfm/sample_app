f6b9aff5214cf0b22986258032bc34c9d7f8df8f
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-04 11:16:47 +0900

    change after_fork code from production.rb to session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
 SampleApp::Application.config.session_store :cookie_store, key: '_sample_app_session'
+
+after_fork do |server, worker|
+  if defined?(ActiveSupport::Cache::Dallistore) and Rails.cache.is_a?(ActiveSupport::Cache::Dallistore)
+    Rails.cache.reset
+
+    ObjectSpace.each_object(ActionDispache::Session::DalliStore) { |obj| obj.reset }
+  end
+end
+
