1381f0bd83f388e773e39a764940135ceedfe416
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-04 11:58:15 +0900

    cache expired after 20 minutes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 SampleApp::Application.config.session_store :cookie_store, key: '_sample_app_session'
+
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
