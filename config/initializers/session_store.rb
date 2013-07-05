07c693f5106f03d1e640497763cfe80329758339
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-05 11:17:44 +0900

    Remove unneed line

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SampleApp::Application.config.session_store :cookie_store, key: '_sample_app_session'
-
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
+SampleApp::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
