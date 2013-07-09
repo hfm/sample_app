ad5a3718c45ce8be1fc22d7c30d0ee2018d7f989
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-09 17:27:40 +0900

    Production環境で、cookiesの有効なドメインを.okkun.pbに指定

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,11 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
+    if Rails.env.production?
+      cookies.permanent[:remember_token] = { :value => user.remember_token, :domain => '.okkun.pb'}
+    else
+      cookies.permanent[:remember_token] = user.remember_token
+    end
     self.current_user = user
   end
 
